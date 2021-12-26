b57bfe3535c789200947e05cc3f16ff70da5587c
maximore32 <78425799+maximore32@users.noreply.github.com>
2021-12-26 17:40:54 -0300

    ok

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-const conexion = "mongodb+srv://maximusbrain:defensa143@cluster0.6di0t.mongodb.net/testmax1?retryWrites=true&w=majority";
+const conexion = process.env.MONGOURL;
 mongoose.connect(conexion ,{useNewUrlParser: true, useUnifiedTopology: true});
